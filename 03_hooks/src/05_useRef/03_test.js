65375eb3471800f510597105dbd3bcaff9cdeda3
kkandda <diobobo@naver.com>
2024-09-27 17:40:58 +0900

    게시판 만들기 중..

--- 03_hooks/src/05_useRef/03_test.js
+++ 03_hooks/src/05_useRef/03_test.js
@@ -4,12 +4,43 @@
     인풋과 텍스트 에어리어를 이용하여 제목과 내용을 받고
     버튼을 이용하여 제목과 내용을 합쳐 게시글로 올린다.
     <textarea/>
+    
 
-    게시된 글들은 사용자가 인풋으로 입력한 글과 대조하여 검색할 수 있다.
+    게시된 글은 사용자가 인풋으로 입력한 글과 대조하여 검색할 수 있다.
     (includes 함수)
+
+    게시된 글은 버튼을 이용해 삭제할 수도 있다.
+    (filter 함수)
 */
 
 import { useState } from "react"
+
+const Gesigul =({gul, index, posts, setPosts})=>{
+    const [gumsak, setGumsak]= useState('');
+
+        // //
+        // const removeTodo=()=>{
+        //     const result = todoList.filter(item =>item !== todo);
+        //     setTodoList(result);
+        // }
+        // //
+
+        const onClickHandlerDelete=()=>{
+            const deletedResults = posts.filter(item => item.title !== gul.title)
+            setPosts(deletedResults)
+        }
+
+    return(
+        <>
+            <li key={index}>{gul.title}</li>
+            <p key={index}>{gul.contents}</p>
+            <button onClick={onClickHandlerDelete}>게시글 삭제</button>
+            <br/>
+            <input type="text" name="search" onClick={onClickHandlerSearch}/>
+        </>
+    )
+    
+}
 
 const Gesipan =()=>{
     const [gesigul, setGesigul] = useState(
@@ -22,13 +53,19 @@ const Gesipan =()=>{
     const [posts, setPosts] = useState([]);
 
     const onChangeHandlerTilte =(e)=>{setGesigul({...gesigul, [e.target.name] : e.target.value})};
-    // {}객체 안에서의 ...연산자, 객체 안의 요소들을 펼쳤다.
+    // {} 객체 안에서의 ...연산자, 객체 안의 요소들을 펼쳤다.
     const onChangeHandlerContents=(e)=>{setGesigul({...gesigul, [e.target.name] : e.target.value})};
 
     const onClickHandlerPost =()=>{
         setPosts([...posts,gesigul])
-        // []배열 안에서의 ...연산자, 기존에 있던 배열과 추가하고자 하는 배열을 더해서 set에 넣었다.
+        // [] 배열 안에서의 ...연산자, 기존에 있던 배열과 추가하고자 하는 배열을 더해서 set에 넣었다.
         // 그렇지 않으면 기존 배열은 삭제된다.
+        setGesigul(
+            {
+                title:"",
+                contents:""
+            }
+        )
     };
     return(
         <>
@@ -55,8 +92,12 @@ const Gesipan =()=>{
             <ul>
                 {posts.map((gul, index)=>(
                     <>
-                    <li key={index}>{gul.title}</li>
-                    <p key={index}>{gul.contents}</p>
+                    <Gesigul gul={gul} index={index} posts={posts} setPosts={setPosts}/>
+                        {/*
+                            <li key={index}>{gul.title}</li>
+                            <p key={index}>{gul.contents}</p>
+                            <button onClick={onClickHandlerDelete}>게시글 삭제</button>
+                        */}
                     </>
                 ))}
             </ul>
